12c87f2e4374befe91d14154ab8e10bc7ddb3318
Morgan Curley <morgan.curley@gmail.com>
2013-09-02 18:26:53 -0400

    Added HTTP_Echo b/c errors are driving me nuts

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -8,6 +8,7 @@ PCRE_VERSION=8.21
 HUK_VERSION=d7643c291ef0
 SR_CACHE_VERSION=0.22
 REDIS_VERSION=0.3.6
+ECHO_VERSION=0.46
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 #pcre_tarball_url=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${PCRE_VERSION}.tar.bz2
@@ -15,13 +16,14 @@ pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION
 http_upstream_keepalive_url=http://mdounin.ru/hg/ngx_http_upstream_keepalive/archive/${HUK_VERSION}.tar.gz
 srcache_tarball_url=https://github.com/agentzh/srcache-nginx-module/archive/v${SR_CACHE_VERSION}.tar.gz
 redis_tarball_url=http://people.freebsd.org/~osa/ngx_http_redis-${REDIS_VERSION}.tar.gz
+echo_tarball_url=https://codeload.github.com/agentzh/echo-nginx-module/tar.gz/v${ECHO_VERSION}
 
 temp_dir=$(mktemp -d /tmp/vulcan_nginx.XXXXXXXXXX)
 
 cleanup() {
   echo "Cleaning up $temp_dir"
-  cd /
-  rm -rf "$temp_dir"
+#  cd /
+#  rm -rf "$temp_dir"
 }
 trap cleanup EXIT
 
@@ -45,12 +47,16 @@ curl $pcre_tarball_url | tar xf -
 echo "Downloading $redis_tarball_url"
 curl $redis_tarball_url | tar xf -
 
+echo "Downloading $echo_tarball_url"
+curl $echo_tarball_url | tar xzf -
+
 cd ..
 
 vulcan build -o ${vulcan_archive_result} -s nginx-${NGINX_VERSION} -v -p /tmp/nginx -c \
     "./configure \
         --with-pcre=pcre-${PCRE_VERSION} \
         --add-module=ngx_http_redis-${REDIS_VERSION} \
+        --add-module=echo-nginx-module-${ECHO_VERSION} \
         --prefix=/tmp/nginx && \
         make install \
     "
