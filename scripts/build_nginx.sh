eb8c1d298e5706321e80542097008c2320bc7287
Morgan Curley <morgan.curley@gmail.com>
2013-09-02 13:00:32 -0400

    Adding SRCache and switching to simplete HttpRedis module

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -6,11 +6,15 @@
 NGINX_VERSION=1.5.2
 PCRE_VERSION=8.21
 HUK_VERSION=d7643c291ef0
+SR_CACHE_VERSION=0.22
+REDIS_VERSION=0.3.6
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 #pcre_tarball_url=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${PCRE_VERSION}.tar.bz2
 pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
 http_upstream_keepalive_url=http://mdounin.ru/hg/ngx_http_upstream_keepalive/archive/${HUK_VERSION}.tar.gz
+srcache_tarball_url=https://github.com/agentzh/srcache-nginx-module/archive/v${SR_CACHE_VERSION}.tar.gz
+redis_tarball_url=http://people.freebsd.org/~osa/ngx_http_redis-${REDIS_VERSION}.tar.gz
 
 temp_dir=$(mktemp -d /tmp/vulcan_nginx.XXXXXXXXXX)
 
@@ -41,12 +45,20 @@ curl $pcre_tarball_url | tar xf -
 echo "Downloading $http_upstream_keepalive_url"
 curl $http_upstream_keepalive_url | tar xf -
 
+echo "Downloading $srcache_tarball_url"
+curl $srcache_tarball_url | tar xf -
+
+echo "Downloading $redis_tarball_url"
+curl $redis_tarball_url | tar xf -
+
 
 
 vulcan build -o ${vulcan_archive_result} -s nginx-${NGINX_VERSION} -v -p /tmp/nginx -c \
     "./configure \
         --with-pcre=pcre-${PCRE_VERSION} \
         --add-module=ngx_http_upstream_keepalive-${HUK_VERSION} \
+        --add-module=srcache-nginx-module-${SR_CACHE_VERSION} \
+        --add-module=ngx_http_redis-${REDIS_VERSION} \
         --prefix=/tmp/nginx && \
         make install \
     "
